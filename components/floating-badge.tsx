855765a82904b7780c9717feb8de274bf54af400
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-23 14:47:06 -0400

    border beam

--- components/floating-badge.tsx
+++ components/floating-badge.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import React from 'react';
+import { BorderBeam } from './magicui/border-beam';
 
 const FloatingBadge = () => {
   return (
     <Link href="https://starter.rasmic.xyz" target='_blank' className="fixed bottom-4 right-4 bg-black border text-white px-3 py-2 rounded-full shadow-lg text-xs font-semibold z-50 hover:bg-zinc-800 transition-colors duration-300">
       Built using NextJS Starter Kit
+      <BorderBeam size={60} duration={4} delay={3} />
     </Link>
   );
 };

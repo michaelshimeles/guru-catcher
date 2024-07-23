aebb3cb415e663b618b03b422d74f7611ed53669
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-22 20:14:10 -0400

    Changed shareable component

--- components/share-revenue.tsx
+++ components/share-revenue.tsx
@@ -1,9 +1,16 @@
 "use client"
-import React from 'react'
-import { Button } from './ui/button'
-import { RevenueData } from '@/utils/types'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import { storeRevenue } from '@/utils/actions/store-revenue'
-import { useRouter } from 'next/navigation'
+import { RevenueData } from '@/utils/types'
+import { Clipboard } from 'lucide-react'
+import { useState } from 'react'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { toast } from "sonner"
 
 function ShareRevenue({
   sales_7_days,
@@ -12,20 +19,44 @@ function ShareRevenue({
   sales_all_days,
   chart_data,
 }: RevenueData) {
+  const [shareUrl, setShareUrl] = useState<string>("")
 
-  const router = useRouter()
+  const copylink = (e: any) => {
+    navigator.clipboard.writeText(shareUrl)
+    toast("Shareable URL has been copied")
+}
+
   return (
-    <Button onClick={async () => {
-      const { url } = await storeRevenue({
-        sales_7_days, sales_30_days,
-        sales_90_days,
-        sales_all_days,
-        chart_data,
-      })
+    <Popover>
+      <PopoverTrigger>
+        <Button onClick={async () => {
+          const { url } = await storeRevenue({
+            sales_7_days, sales_30_days,
+            sales_90_days,
+            sales_all_days,
+            chart_data,
+          })
 
-      router.push(`/dashboard/${url}`)
+          setShareUrl(`${process.env.NEXT_PUBLIC_BASE_DOMAIN}/dashboard/${url}`)
 
-    }}>Share Revenue</Button>
+        }}>Share Revenue</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="space-y-1">
+          <h4 className="font-medium leading-none">Shareable URL</h4>
+          <p className="text-sm text-muted-foreground">
+            You can share this url
+          </p>
+        </div>
+        <div className='flex gap-1 mt-2'>
+          <Input value={shareUrl} className='text-xs'/>
+          <Button size="icon" variant="outline" onClick={copylink}>
+            <Clipboard className='w-4 h-4'/>
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+
   )
 }
 
